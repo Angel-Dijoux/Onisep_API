39f235a1d212f234ae8880a183d2a871ec2c2cd6
Angel Dijoux <angel.dijoux@yahoo.com>
2023-04-21 19:31:08 +0200

    log the function

--- utils/get_formations_xml.py
+++ utils/get_formations_xml.py
@@ -3,17 +3,37 @@ import zipfile
 import json
 import io
 import xmltodict
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s:%(levelname)s:%(message)s")
 
 # Télécharger le fichier zip
 url = "https://api.opendata.onisep.fr/tmp/d1/aa/f8ec0c12fcb8e7908dde50613b44/ideo-fiches_formations.zip"
 response = requests.get(url)
 
+
 # Extraire le fichier XML du zip
-with zipfile.ZipFile(io.BytesIO(response.content)) as myzip:
-    with myzip.open(myzip.namelist()[0]) as myfile:
-        data_dict = xmltodict.parse(myfile.read())
+def extract_xml(response: requests) -> dict:
+    with zipfile.ZipFile(io.BytesIO(response.content)) as myzip:
+        with myzip.open(myzip.namelist()[0]) as myfile:
+            logging.debug("File {} is extract to .XML.".format(myzip.namelist()[0]))
+            return xmltodict.parse(myfile.read())
+
 
 # Convertir le fichier XML en JSON
-json_data = json.dumps(data_dict)
-with open("assets/formation/data.json", "w") as json_file:
-    json_file.write(json_data)
+def convert_to_xml_to_json(xml: dict) -> bool:
+    json_data = json.dumps(xml)
+    filename = "data.json"
+    path = "assets/formation/"
+    try:
+        with open(path + filename, "w") as json_file:
+            json_file.write(json_data)
+            logging.debug("File {} is write in {}".format(filename, path))
+            return True
+    except Exception:
+        logging.debug("Error in convert : " + Exception)
+        return False
+
+
+if __name__ == "__main__":
+    convert_to_xml_to_json(extract_xml(response))
