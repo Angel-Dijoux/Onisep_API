c7dca2e6b17220538a0227157b1a35f9d1c0fc5e
Angel Dijoux <angel.dijoux@yahoo.com>
2023-04-21 19:50:04 +0200

    modified:   src/formations.py

--- src/formations.py
+++ src/formations.py
@@ -6,19 +6,26 @@ from typing import Any
 
 from src.constants.http_status_codes import (
     HTTP_200_OK,
+    HTTP_404_NOT_FOUND,
+    HTTP_500_INTERNAL_SERVER_ERROR,
 )
 
 formations = Blueprint("formations", __name__, url_prefix="/api/v1/formations")
 
 
-def filter_by_link(formations: list[dict[str, Any]], id: str) -> list[dict[str, Any]]:
-    return list(filter(lambda f: f["identifiant"] == id, formations))
+def filter_by_link(formations: list[dict[str, Any]], id: str) -> dict[str, Any]:
+    filtered_list = list(filter(lambda f: f["identifiant"] == id, formations))
+    return filtered_list[0] if filtered_list else []
 
 
 @formations.route("/<string:id>")
 @swag_from("./docs/formations/formation.yaml")
 def get_formation_by_id(id: str) -> "Response":
     print(id)
-    with open("assets/formation/data.json", "r") as json_file:
-        data = json.load(json_file)
-    return jsonify(filter_by_link(data["formations"]["formation"], id)), HTTP_200_OK
+    try:
+        with open("assets/formation/data.json", "r") as json_file:
+            result = filter_by_link(json.load(json_file)["formations"]["formation"], id)
+        return jsonify(result), HTTP_200_OK if len(result) > 0 else HTTP_404_NOT_FOUND
+    except Exception:
+        print("Error in get_formation_by_id : ", Exception)
+        return HTTP_500_INTERNAL_SERVER_ERROR
