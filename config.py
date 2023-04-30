fcd8bb633ec8d0002464a4ffec6d6fbc648687aa
Angel Dijoux <angel.dijoux@yahoo.com>
2023-05-01 01:54:34 +0200

    modified:   config.py

--- config.py
+++ config.py
@@ -12,6 +12,7 @@ def get_prod_db_uri() -> str:
     username = os.environ.get("DATABASE_USERNAME")
     password = os.environ.get("DATABASE_PASSWORD")
     host = os.environ.get("DATABASE_HOST")
+    print(host, "LOG FROM SERVER")
     return f"mysql+mysqlconnector://{username}:{password}@{host}/onisep"
 
 
@@ -22,7 +23,7 @@ class Config(object):
     CSRF_ENABLED = os.environ.get("CSRF_ENABLED", True)
     CORS_HEADERS = os.environ.get("CORS_HEADERS")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+    JWT_SECRET_KEY = SECRET_KEY
 
     SWAGGER = {"title": "Onisep_User API", "uiversion": "3", "version": "1.0.4"}
 
