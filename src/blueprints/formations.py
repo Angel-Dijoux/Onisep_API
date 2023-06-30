ae9d72ac91e3a01be3e465f26f505ae32fa67888
Angel Dijoux <angel.dijoux@yahoo.com>
2023-06-30 16:59:27 +0200

    disable

--- src/blueprints/formations.py
+++ src/blueprints/formations.py
@@ -7,7 +7,6 @@ from typing import Any, Tuple
 
 from src.constants.http_status_codes import (
     HTTP_200_OK,
-    HTTP_404_NOT_FOUND,
     HTTP_500_INTERNAL_SERVER_ERROR,
 )
 
@@ -25,7 +24,7 @@ def get_formation_by_id(id: str) -> Tuple[Response, int] | HTTPException:
     try:
         with open("assets/formation/data.json", "r") as json_file:
             result = filter_by_link(json.load(json_file)["formations"]["formation"], id)
-        return jsonify(result), HTTP_200_OK if len(result) > 0 else HTTP_404_NOT_FOUND
+        return jsonify(result), HTTP_200_OK if len(result) > 0 else HTTP_200_OK
     except Exception:
         print("Error in get_formation_by_id : ", Exception)
         abort(HTTP_500_INTERNAL_SERVER_ERROR)
