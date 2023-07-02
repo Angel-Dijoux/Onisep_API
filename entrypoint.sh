db6774aa4598ee5f2ba722190b358db89c1a6ff4
Angel Dijoux <angel.dijoux@yahoo.com>
2023-07-03 00:43:59 +0200

    ltl edit

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,6 @@ if [ ! -d "/app/migrations" ]; then
   echo "Init database"
   flask db init
 fi
-echo "Run migration"
-flask db migrate -m 'create initial tables'
 echo "Upgrade tables"
 flask db upgrade
 echo "Finish"
