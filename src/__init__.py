68ebac6c0dc149e9b241499c1ddc5ab853aa3069
Angel-Dijoux <angel.dijoux@yahoo.com>
2023-10-26 22:11:08 +0200

    feat: init scrap.

--- src/__init__.py
+++ src/__init__.py
@@ -1,22 +1,25 @@
-from flask import Flask
-from flask_jwt_extended import JWTManager
-from flask_cors import CORS
+import sys
+
 from flasgger import Swagger
+from flask import Flask
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from .middlewares import after_request
-from .errors import register_error_handlers
-
-from src.config.swagger import template, swagger_config
+from loguru import logger
 
 from config import DevelopmentConfig, ProductionConfig
-
+from src.config.swagger import swagger_config, template
 from src.constants.env import is_dev
+
+from .errors import register_error_handlers
+from .middlewares import after_request
 
 db = SQLAlchemy()
 
 
 def create_app(config_class=DevelopmentConfig):
+    _setup_logging()
     app = Flask(__name__, instance_relative_config=True)
 
     if not is_dev():
@@ -39,12 +42,31 @@ def create_app(config_class=DevelopmentConfig):
 
 
 def register_blueprints(app: Flask):
+    from src.blueprints.auth import auth
     from src.blueprints.favoris import favoris
     from src.blueprints.formations import formations
-    from src.blueprints.auth import auth
     from src.blueprints.utils import utils
 
     app.register_blueprint(utils)
     app.register_blueprint(auth)
     app.register_blueprint(favoris)
     app.register_blueprint(formations)
+
+
+def _setup_logging():
+    logger.remove()
+    logger.level("INFO", color="<green>")
+    logger.level("DEBUG", color="<blue>")
+    logger.level("WARNING", color="<yellow>")
+    logger.level("ERROR", color="<red>")
+    logger.level("CRITICAL", color="<red>")
+    logger.add(
+        sys.stderr,
+        colorize=True,
+        format=(
+            "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
+            "<level>{level: <8}</level> | "
+            "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level> | "
+            "{extra}"
+        ),
+    )
