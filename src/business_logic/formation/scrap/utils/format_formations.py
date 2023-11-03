1fa8d95cb5fb9a98ca3d56c3725f43b406bc83da
Angel Dijoux <77701490+Angel-Dijoux@users.noreply.github.com>
2023-11-03 13:03:27 +0100

    [Formation] Add `is_favorite` information in formation data. (#17)
    feat: now we can see if formation `is_favirite` or not.

---
+++ src/business_logic/formation/scrap/utils/format_formations.py
@@ -0,0 +1,35 @@
+from src.business_logic.favoris.is_favorite import check_if_is_favorite
+from src.business_logic.formation.scrap.types import (
+    FormationIsFavortite,
+)
+from src.models.formation import Formation
+
+
+def _create_formation_from_dict(formation: dict) -> Formation:
+    return Formation(
+        code_nsf=int(formation.get("code_nsf") or 0),
+        type=formation.get("sigle_type_formation")
+        or formation.get("libelle_type_formation"),
+        libelle=formation.get("libelle_formation_principal"),
+        tutelle=formation.get("tutelle"),
+        url=formation.get("url_et_id_onisep"),
+        domain=formation.get("domainesous-domaine"),
+        niveau_de_sortie=formation.get("niveau_de_sortie_indicatif"),
+        duree=formation.get("duree"),
+    )
+
+
+def format_formations(data: list[dict]) -> list[Formation]:
+    return [_create_formation_from_dict(formation).to_dict() for formation in data]
+
+
+def format_formation_with_is_favorite(
+    user_id: int, data: list[dict]
+) -> list[FormationIsFavortite]:
+    return [
+        FormationIsFavortite(
+            formation=_create_formation_from_dict(formation).to_dict(),
+            is_favorite=check_if_is_favorite(user_id, formation["url_et_id_onisep"]),
+        )
+        for formation in data
+    ]
