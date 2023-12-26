6c02c40ff324dad1856e7b51e997b388bd10d35d
Angel Dijoux <77701490+Angel-Dijoux@users.noreply.github.com>
2023-12-26 17:57:12 +0100

    [Formation] Update exeption error. (#18)
    feat : upgrade error message

--- src/business_logic/formation/scrap/utils/get_onisep_data.py
+++ src/business_logic/formation/scrap/utils/get_onisep_data.py
@@ -2,6 +2,7 @@
 # https://opendata.onisep.fr/data/5fa591127f501/2-ideo-formations-initiales-en-france.htm
 import requests
 from src.business_logic.formation import HEADERS, ONISEP_URL
+from src.business_logic.formation.exceptions import NoOnisepAPIException
 
 
 DATASET = "5fa591127f501"
@@ -12,4 +13,6 @@ def get_onisep_data(params: str) -> dict:
     response = requests.get(url, headers=HEADERS)
     if response.status_code == 200:
         return response.json()
-    raise Exception("Onisep API is down.", response.status_code)
+    raise NoOnisepAPIException(
+        f"status: {response.status_code} \n message : Onisep API is down.  \n dataset : {DATASET} \n headers : {HEADERS} "
+    )
