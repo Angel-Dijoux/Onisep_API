ba0da870ff36087190fb1618b8e63ceaba3b12a2
Angel-Dijoux <angel.dijoux@yahoo.com>
2024-01-01 02:30:27 +0100

    fix: remove settings from `__init__.py`

--- src/business_logic/formation/scrap/utils/get_onisep_data.py
+++ src/business_logic/formation/scrap/utils/get_onisep_data.py
@@ -1,14 +1,30 @@
 # Idéo-Formations initiales en France
 # https://opendata.onisep.fr/data/5fa591127f501/2-ideo-formations-initiales-en-france.htm
+from enum import Enum
+import os
 import requests
-from src.business_logic.formation import HEADERS, ONISEP_URL, HeaderKey
 from src.business_logic.formation.exceptions import NoOnisepAPIException
-from src.business_logic.formation.scrap.utils.get_onisep_token import get_token
+from src.business_logic.formation.scrap.utils.get_onisep_token import (
+    BearerToken,
+    get_token,
+)
 from src.constants.http_status_codes import (
     HTTP_200_OK,
     HTTP_401_UNAUTHORIZED,
 )
 
+
+class HeaderKey(Enum):
+    APPLICATION_ID = "Application-ID"
+    AUTHORIZATION = "Authorization"
+
+
+ONISEP_URL = "https://api.opendata.onisep.fr/api/1.0/dataset/"
+
+HEADERS: dict[HeaderKey, BearerToken | str] = {
+    HeaderKey.APPLICATION_ID.value: os.environ.get("ONISEP_APP_ID"),
+    HeaderKey.AUTHORIZATION.value: get_token(),
+}
 
 DATASET = "5fa591127f501"
 
