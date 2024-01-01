4e4362e30f3cb2c3415f55227b94996975f8ad0d
Angel Dijoux <77701490+Angel-Dijoux@users.noreply.github.com>
2024-01-01 02:31:47 +0100

    Fix: App is blocked when onisep is down (#23)
    fix: remove settings from `__init__.py`

--- src/business_logic/formation/scrap/utils/get_onisep_token.py
+++ src/business_logic/formation/scrap/utils/get_onisep_token.py
@@ -1,12 +1,15 @@
 import os
 from typing import Literal
 import requests
+from urllib3 import Retry
 
 from src.business_logic.formation.exceptions import NoOnisepAPIException
 from src.constants.http_status_codes import HTTP_200_OK
-
+from requests.adapters import HTTPAdapter
 
 URL = "https://api.opendata.onisep.fr/api/1.0/login"
+RETRY = 3
+ERROR_STATUS = [500, 502, 503, 504]
 
 
 def _get_form_data() -> dict[str, str]:
@@ -16,14 +19,37 @@ def _get_form_data() -> dict[str, str]:
     }
 
 
+def _send_request(session: requests.Session) -> requests.Response:
+    response = session.post(URL, data=_get_form_data())
+    return response
+
+
 BearerToken = Literal["Bearer"]
 
 
 def get_token() -> BearerToken:
-    response = requests.post(URL, data=_get_form_data())
-    if response.status_code == HTTP_200_OK:
-        data = response.json()
-        return f"Bearer {data.get('token')}"
-    raise NoOnisepAPIException(
-        f"Failed to get onisep token. Status code: {response.status_code}"
+    session = create_retry_session()
+    for _ in range(RETRY):
+        response = _send_request(session)
+        if response.status_code == HTTP_200_OK:
+            data = response.json()
+            return f"Bearer {data.get('token')}"
+        elif response.status_code in ERROR_STATUS:
+            pass
+        else:
+            raise NoOnisepAPIException(
+                f"Failed to get onisep token. Status code: {response.status_code}"
+            )
+    raise NoOnisepAPIException("Maximum number of retry attempts reached.")
+
+
+def create_retry_session() -> requests.Session:
+    session = requests.Session()
+    retry_strategy = Retry(
+        total=RETRY,
+        backoff_factor=1,
+        status_forcelist=ERROR_STATUS,
     )
+    adapter = HTTPAdapter(max_retries=retry_strategy)
+    session.mount("https://", adapter)
+    return session
